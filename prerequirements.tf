0271acbb1da8c18dbddbe5f916cdcb8a5a856dd9
Marcel Sinn <argonqq@mailbox.org>
2021-02-14 17:53:05 +0100

    Ignore labels annotations (#3)
    * Ignore labels and annotations
    
    * Fix lifecycle block

--- prerequirements.tf
+++ prerequirements.tf
@@ -2,8 +2,8 @@ resource "kubernetes_namespace" "rancher" {
   count = var.enable_rancher ? 1 : 0
   metadata {
     name = var.rancher_namespace
-    lifecycle {
-      ignore_changes = [metadata[0].annotations, metadata[0].labels]
-    }
+  }
+  lifecycle {
+    ignore_changes = [metadata[0].annotations, metadata[0].labels]
   }
 }
